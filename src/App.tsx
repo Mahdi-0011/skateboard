0fd9430ddbab0cf4a61a49193074ecb1e19fcefe
Niklas Mårdby <niklas.mardby@gmail.com>
2025-02-24 11:21:49 +0100

    Updates to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import ListSection from "./components/ListSection/ListSection";
 import "./App.scss";
+import Counter from "./components/Counter/Counter";
 
 function App() {
+	const animals = ["gorilla", "horse", "pig", "giraff", "chicken", "elephant"];
+	const peoples = ["Karen", "Bobby", "Nisse", "Stina"];
+	const fruits = ["apple", "orange"];
+
 	return (
 		<>
-			<h1>skateboard</h1>
+			<h1>🛹 skateboard</h1>
 			<p>a demo for students of React</p>
+			<Counter />
+			<ListSection items={animals} />
+			<ListSection items={peoples} />
+			<ListSection items={fruits} />
+			<button>Hello?</button>
 		</>
 	);
 }
